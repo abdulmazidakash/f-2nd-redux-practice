11b0ee41eefda8ede295a3f0e393383eba8d2a23
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-03-22 17:33:54 +0600

    counter done

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,60 @@
 
-import { useState } from "react";
+
 import Counter from "./components/Counter";
 import Stats from "./components/Stats";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./features/counters/CountersSlice";
 
-const initialCounters = [
-  {
-    id: 1,
-    value: 0
-  },
-  {
-    id: 2,
-    value: 0
-  },
-]
+// const initialCounters = [
+//   {
+//     id: 1,
+//     value: 0
+//   },
+//   {
+//     id: 2,
+//     value: 0
+//   },
+// ]
 
 export default function App() {
 
-  const [counters, setCounters] = useState(initialCounters);
+  // const [counters, setCounters] = useState(initialCounters);
+  const counters = useSelector(state => state.counters);
+  console.log(counters);
+  const dispatch = useDispatch();
+  console.log(dispatch);
 
   const totalCount = counters.reduce((sum, current)=> sum + current.value, 0);
 
   const handleIncrement = (counterId) =>{
-    const updatedCounters = counters.map(counter => {
-      if (counter.id === counterId) {
-        return {
-          ...counter,
-          value: counter.value + 1,
-        };
-      }; // ❌ REMOVE this extra semicolon
-      return counter;
-    });
+    // const updatedCounters = counters.map(counter => {
+    //   if (counter.id === counterId) {
+    //     return {
+    //       ...counter,
+    //       value: counter.value + 1,
+    //     };
+    //   }; // ❌ REMOVE this extra semicolon
+    //   return counter;
+    // });
     
-    setCounters(updatedCounters);
+    // setCounters(updatedCounters);
+    dispatch(increment(counterId));
   }
 
   const handleDecrement = (counterId) =>{
-    const updatedCounters = counters.map(counter => {
-      if (counter.id === counterId) {
-        return {
-          ...counter,
-          value: counter.value - 1,
-        };
-      }; // ❌ REMOVE this extra semicolon
-      return counter;
-    });
+    // const updatedCounters = counters.map(counter => {
+    //   if (counter.id === counterId) {
+    //     return {
+    //       ...counter,
+    //       value: counter.value - 1,
+    //     };
+    //   }; // ❌ REMOVE this extra semicolon
+    //   return counter;
+    // });
     
 
-      setCounters(updatedCounters);
+    //   setCounters(updatedCounters);
+    dispatch(decrement(counterId))
   }
   return (
     <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import countersReducer from '../features/counters/CountersSlice'
+
+const store = configureStore({
+	reducer: {
+		counters: countersReducer,
+	}
+});
+
+export default store;
