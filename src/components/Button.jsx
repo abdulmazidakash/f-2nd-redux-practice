fb959dbf539e8cabcada2ef837c2ad7b418fc903
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-03-22 15:22:40 +0600

    commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
-export default function Button({ handler, label }) {
+export default function Button({ handler, type, label }) {
 	return (
-	  <button
-		onClick={handler}
-		className="px-4 py-2 btn btn-primary text-white rounded-md"
-	  >
-		{label}
-	  </button>
+		<button
+			onClick={handler}
+			className={`px-4 py-2 btn ${
+				type === "btn-success" ? "btn-success" : "btn-error"
+			} text-white rounded-md`}
+		>
+			{label}
+		</button>
 	);
-  }
-  
+}
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,7 +7,7 @@ export default function Counter({ count, onIncrement, onDecrement}) {
     <div className="bg-rose-100 p-5 rounded-lg shadow-md text-center">
       <h2 className="text-xl font-semibold">Count: {count}</h2>
       <div className="mt-4 flex justify-center space-x-4">
-        <Button className={'btn btn-accent'} handler={onIncrement} label={'Increment'} />
+        <Button type={'btn-success'} className={'btn btn-accent'} handler={onIncrement} label={'Increment'} />
         {/* <Button onClick={() => setCount(0)} label="Reset" /> */}
         <Button handler={onDecrement} label={'Decrement'} />
       </div>
